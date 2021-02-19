14ec98d76ff64aaa926d900f80226706abaa6634
Christoper <christoper@go-jek.com>
2021-02-19 17:26:42 +0100

    Change test suite var msr to repo

--- service/student_test.go
+++ service/student_test.go
@@ -12,38 +12,38 @@ import (
 
 type StudentServiceSuite struct {
 	suite.Suite
-	msr     *mocks.StudentRepository
+	repo    *mocks.StudentRepository
 	service *Student
 }
 
 func (suite *StudentServiceSuite) SetupTest() {
-	suite.msr = &mocks.StudentRepository{}
-	suite.service = &Student{suite.msr}
+	suite.repo = &mocks.StudentRepository{}
+	suite.service = &Student{suite.repo}
 }
 
 func (suite *StudentServiceSuite) TestGetStudent() {
 	s := factory.NewStudent()
-	suite.msr.On("GetStudent", 1).Return(s, nil)
+	suite.repo.On("GetStudent", 1).Return(s, nil)
 	rs, err := suite.service.Get(1)
 	assert.Nil(suite.T(), err)
 	assert.Equal(suite.T(), s, rs)
 }
 
 func (suite *StudentServiceSuite) TestDeleteStudent() {
-	suite.msr.On("DeleteStudent", 1).Return(nil)
+	suite.repo.On("DeleteStudent", 1).Return(nil)
 	err := suite.service.Delete(1)
 	suite.Nil(err)
-	suite.msr.AssertExpectations(suite.T())
+	suite.repo.AssertExpectations(suite.T())
 }
 
 func (suite *StudentServiceSuite) TestEditStudent() {
 	s := factory.NewStudent()
 
-	suite.msr.On("EditStudent", &s).Return(nil)
+	suite.repo.On("EditStudent", &s).Return(nil)
 
 	err := suite.service.Edit(&s)
 	suite.Nil(err)
-	suite.msr.AssertExpectations(suite.T())
+	suite.repo.AssertExpectations(suite.T())
 }
 
 func TestStudentService(t *testing.T) {
@@ -53,11 +53,11 @@ func TestStudentService(t *testing.T) {
 func (suite *StudentServiceSuite) TestAddStudent() {
 	s := factory.NewStudent()
 
-	suite.msr.On("AddStudent", &s).Return(nil)
+	suite.repo.On("AddStudent", &s).Return(nil)
 
 	err := suite.service.Add(&s)
 	suite.Nil(err)
-	suite.msr.AssertExpectations(suite.T())
+	suite.repo.AssertExpectations(suite.T())
 }
 
 func (suite *StudentServiceSuite) TestAddStudentExceedingCreditLimit() {
@@ -70,6 +70,6 @@ func (suite *StudentServiceSuite) TestAddStudentExceedingCreditLimit() {
 
 	err := suite.service.Add(&s)
 	suite.Equal(err, OverCreditErr, "Should return OverCreditErr")
-	suite.msr.AssertNotCalled(suite.T(), "AddStudent")
+	suite.repo.AssertNotCalled(suite.T(), "AddStudent")
 
 }
