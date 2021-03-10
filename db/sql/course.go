87c47a35c8b820fe708f942fde9bd50465d78e99
Christoper <christoper@go-jek.com>
2021-03-10 20:56:52 +0100

    Implement sql Get Students

--- db/sql/course.go
+++ db/sql/course.go
@@ -1,10 +1,72 @@
 package sql
 
-import "gorm.io/gorm"
+import (
+	"strconv"
+	"time"
+
+	"github.com/ctoto93/demo"
+	"github.com/mitchellh/mapstructure"
+)
 
 type Course struct {
-	gorm.Model
-	Name     string   `json:"name"`
-	Credit   int      `json:"credit"`
-	Students []Course `json:"students,omitempty" gorm:"many2many:student_courses;"`
+	Id        int        `gorm:"primarykey"`
+	Name      string     `json:"name"`
+	Credit    int        `json:"credit"`
+	Students  *[]Student `json:"courses,omitempty" gorm:"many2many:course_courses;"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (c *Course) toDemo() demo.Course {
+	return demo.Course{
+		Id:       strconv.Itoa(c.Id),
+		Name:     c.Name,
+		Credit:   c.Credit,
+		Students: make([]demo.Student, 0),
+	}
+}
+
+func newCourse(in interface{}) (Course, error) {
+	var c Course
+	if err := mapstructure.Decode(in, &c); err != nil {
+		return Course{}, err
+	}
+
+	students := make([]Student, 0)
+	c.Students = &students
+
+	return c, nil
+}
+
+func (r *repository) GetCourse(sid string) (demo.Course, error) {
+	id, err := strconv.Atoi(sid)
+	if err != nil {
+		return demo.Course{}, err
+	}
+
+	var c Course
+
+	if err := r.db.First(&c, id).Error; err != nil {
+		return demo.Course{}, err
+	}
+
+	return c.toDemo(), nil
+
+}
+
+func (r *repository) AddCourse(dc *demo.Course) error {
+
+	c, err := newCourse(dc)
+	if err != nil {
+		return err
+	}
+
+	if err := r.db.Create(&c).Error; err != nil {
+		return err
+	}
+
+	dc.Id = strconv.Itoa(c.Id)
+
+	return nil
+
 }
--- db/sql/student.go
+++ db/sql/student.go
@@ -1,12 +1,78 @@
 package sql
 
 import (
-	"gorm.io/gorm"
+	"strconv"
+	"time"
+
+	"github.com/ctoto93/demo"
 )
 
 type Student struct {
-	gorm.Model
-	Name    string   `json:"name"`
-	Age     int      `json:"age"`
-	Courses []Course `json:"courses,omitempty" gorm:"many2many:user_languages;"`
+	Id        int       `gorm:"primarykey"`
+	Name      string    `json:"name"`
+	Age       int       `json:"age"`
+	Courses   *[]Course `json:"courses,omitempty" gorm:"many2many:user_languages;"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func newStudent(ds demo.Student) (Student, error) {
+	s := Student{
+		Name: ds.Name,
+		Age:  ds.Age,
+	}
+
+	if ds.Id != "" {
+		id, err := strconv.Atoi(ds.Id)
+		if err != nil {
+			return Student{}, err
+		}
+		s.Id = id
+	}
+
+	courses := make([]Course, 0)
+	s.Courses = &courses
+
+	return s, nil
+}
+
+func (s *Student) toDemo() demo.Student {
+	return demo.Student{
+		Id:      strconv.Itoa(s.Id),
+		Name:    s.Name,
+		Age:     s.Age,
+		Courses: make([]demo.Course, 0),
+	}
+}
+
+func (r *repository) GetStudent(sid string) (demo.Student, error) {
+	id, err := strconv.Atoi(sid)
+	if err != nil {
+		return demo.Student{}, err
+	}
+
+	var s Student
+
+	if err := r.db.First(&s, id).Error; err != nil {
+		return demo.Student{}, err
+	}
+
+	return s.toDemo(), nil
+
+}
+
+func (r *repository) AddStudent(ds *demo.Student) error {
+
+	s, err := newStudent(*ds)
+	if err != nil {
+		return err
+	}
+
+	if err := r.db.Create(&s).Error; err != nil {
+		return err
+	}
+	ds.Id = strconv.Itoa(s.Id)
+
+	return nil
+
 }
